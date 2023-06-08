23c1871318957cf012be68f63f39b7cbf40bf88f
Mim Collopy <mim.collopy@gmail.com>
2023-06-08 13:59:47 +1000

    MVP//WIP

--- script.js
+++ script.js
@@ -1,36 +1,129 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tic-Tac-Toe</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="pageWrapper">
+//name variables
+let squares = document.querySelectorAll(".square")
+let numOfPlays = 0 
+let replayBtn = document.querySelector(".replayBtn")
+let playerTurnSpan = document.querySelector(".playerTurn")
+let countSpan = document.querySelector(".counter")
 
-        <header>
-            <h1>Tic-Tac-Toe</h1>
-        </header>
 
-        <div id="board">
-            
-                <div class="square1">1 </div>
-                <div class="square2">2</div>
-                <div class="square3">3</div>
-                <div class="square4">4</div>
-                <div class="square5">5</div>
-                <div class="square6">6</div>
-                <div class="square7">7</div>
-                <div class="square8">8</div>
-                <div class="square9">9</div>
-            
+for (let i = 0; i < squares.length; i++){
+    squares[i].setAttribute("data-number", i);
+}
 
+let pinkSquaresStr = [];
+let greenSquaresStr = [];
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+// listen for events
+for (let square of squares){
+    square.addEventListener("click", handlePlay)
+}
+replayBtn.addEventListener("click", handleReplay)
+// // add some functions
+function isEven(){
+    return(numOfPlays % 2 === 0)
+}
+
+function handlePlay(event){
+    if(isEven()){
+         event.target.style.backgroundColor = "var(--even-bg)";
+         pinkSquaresStr.push(event.target.getAttribute("data-number"));
+         
+    } else{
+        event.target.style.backgroundColor = "var(--odd-bg)"
+        greenSquaresStr.push(event.target.getAttribute("data-number"));
+    }
+    numOfPlays ++
+    hasWon()
+    hasDraw()
+    countSpan.innerText = numOfPlays
+    
+    if (isWinner === false && isDraw === false){
+    
+        if(isEven()) {
+            playerTurnSpan.innerText = "pink's turn";
+        } else{
+            playerTurnSpan.innerText = "green's turn";
+        }
+    }
+    event.target.removeEventListener("click", handlePlay);
+    
+}
+let isWinner = false
+let isDraw = false
+function hasWon(){
+    for (let winArr of winningCombos){
+        let pinkCount = 0
+        for (num of winArr){
+            let pinkSquaresArr = pinkSquaresStr.map(Number);
+            if(pinkSquaresArr.includes(num)){
+                pinkCount++
+            } 
+            if (pinkCount === 3){
+                isWinner = true
+                console.log("Pink wins!")
         
+                playerTurnSpan.innerText = ("Pink wins!")
+                for (let square of squares){
+                    square.removeEventListener("click", handlePlay);
+                    } 
+            }
+        }
+        let greenCount = 0
+        for (num of winArr){
+            let greenSquaresArr = greenSquaresStr.map(Number);
+            if(greenSquaresArr.includes(num)){
+                greenCount++
+            } 
+            if (greenCount === 3){
+                isWinner = true
+                console.log("Green wins!")
 
-        </div>
-    </div>
-    <script src="app.js"></script>
-</body>
-</html>
+                playerTurnSpan.innerText = ("Green wins!")
+                for (let square of squares){
+                square.removeEventListener("click", handlePlay);
+                }
+            }
+        }
+
+    }
+}
+
+
+function hasDraw(){
+    if (numOfPlays === 9 && isWinner === false){
+        console.log("it's a tie")
+        isDraw = true
+        playerTurnSpan.innerText = ("It's a tie")
+    return
+
+    }
+}
+
+
+
+function handleReplay(){
+    isDraw = false
+    isWinner = false
+    numOfPlays = 0
+    pinkSquaresStr = []
+    greenSquaresStr = []
+    for (let square of squares){
+    square.removeAttribute("style");
+    square.addEventListener("click", handlePlay)
+
+    }
+
+
+
+}
